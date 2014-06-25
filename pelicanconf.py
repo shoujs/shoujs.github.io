4d72cf1f64bff5a21f7100fbdd9158f28bf35f08
shoujs <shoujiesong@gmail.com>
2014-06-25 14:49:28 +0800

    add cname

--- pelicanconf.py
+++ pelicanconf.py
@@ -29,3 +29,5 @@ DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+STATIC_PATHS = ['images', 'extras/CNAME']
+EXTRA_PATH_METADATA = {'extras/CNAME': {'path': 'CNAME'},}
